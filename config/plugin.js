0e97968a86796ccba80aa3e39c21236b41a087cd
T007 <1255965856@qq.com>
2020-10-26 23:56:03 +0800

    下载文件

---
+++ config/plugin.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+module.exports = {
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
+  sequelize: {
+    enable: true,
+    package: 'egg-sequelize',
+  },
+  valparams: {
+    enable: true,
+    package: 'egg-valparams',
+  },
+  redis: {
+    enable: true,
+    package: 'egg-redis',
+  },
+  nunjucks: {
+    enable: true,
+    package: 'egg-view-nunjucks',
+  },
+  jwt: {
+    enable: true,
+    package: 'egg-jwt',
+  },
+  oss: { enable: true,
+    package: 'egg-oss',
+  },
+};
